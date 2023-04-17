5226876edbcb20edecd94082a631f4dbac28dd0b
RafaelXokito <Rafael_Pereira_2000@hotmail.com>
2023-04-17 01:11:17 +0100

    variables

--- production.tf
+++ production.tf
@@ -1,3 +1,27 @@
+variable "client_image_tag" {
+  type        = string
+  description = "The Docker image_tag for the client."
+  default     = "latest"
+}
+
+variable "server_image_tag" {
+  type        = string
+  description = "The Docker image_tag for the Express server."
+  default     = "latest"
+}
+
+variable "socket_server_image_tag" {
+  type        = string
+  description = "The Docker image_tag for the Socket.io server."
+  default     = "latest"
+}
+
+variable "file_gateway_image_tag" {
+  type        = string
+  description = "The Docker image_tag for the file-gateway server."
+  default     = "latest"
+}
+
 module "production" {
   source = "./modules/production"
   
@@ -11,6 +35,11 @@ module "production" {
   client_image       = "rafaelxokito/fileexchangehubclient"
   file_gateway_image       = "rafaelxokito/fileexchangehubfilegateway"
   mongo_connection_string = "mongodb+srv://admin:admin@fileexchangehub.vfybz5q.mongodb.net/test"
+
+  client_image_tag       = var.client_image_tag
+  server_image_tag       = var.server_image_tag
+  socket_server_image_tag = var.socket_server_image_tag
+  file_gateway_image_tag = var.file_gateway_image_tag
 
   # azure_client_id = ""
   # azure_client_secret = ""
