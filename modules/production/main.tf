3ec8e8cfaec7042b6ac5af28103fb1a4ff22696e
RafaelXokito <Rafael_Pereira_2000@hotmail.com>
2023-04-21 01:43:56 +0100

    Update main.tf

--- modules/production/main.tf
+++ modules/production/main.tf
@@ -106,7 +106,7 @@ resource "google_cloud_run_domain_mapping" "default" {
   }
 
   spec {
-    route_name = google_cloud_run_service.client.name
+    route_name = google_cloud_run_service.client.uri
   }
 
 }
