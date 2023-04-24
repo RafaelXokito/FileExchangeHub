3d2242a20750f804e2399976b48ff4d4de5371a2
RafaelXokito <Rafael_Pereira_2000@hotmail.com>
2023-04-24 12:36:53 +0100

    Update variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "project_id" {
+  default = ""
+}
+variable "client_image" {
+  default = ""
+}
+variable "server_image" {
+  default = ""
+}
+variable "socket_server_image" {
+  default = ""
+}
+variable "file_gateway_image" {
+  default = ""
+}
+variable "mongo_connection_string" {
+  default = ""
+}
