696d63ae4b2ba43952146cafc6a5fbb4961bb5d5
RafaelXokito <Rafael_Pereira_2000@hotmail.com>
2023-04-24 12:49:16 +0100

    variables

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
 module "production" {
     source = "./modules/production"
-    
-    project_id              = var.project_id
-    client_image            = var.client_image
-    server_image            = var.server_image
-    socket_server_image     = var.socket_server_image
-    file_gateway_image      = var.file_gateway_image
-    mongo_connection_string = var.mongo_connection_string
 }
 
 module "development" {
