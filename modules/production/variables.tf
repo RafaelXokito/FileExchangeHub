926e5e6a4e09cb6e3108139185d436d59e1efc8f
RafaelXokito <Rafael_Pereira_2000@hotmail.com>
2023-04-24 11:49:15 +0100

    variables

--- modules/production/variables.tf
+++ modules/production/variables.tf
@@ -1,29 +1,35 @@
 variable "project_id" {
   description = "The project ID to deploy to"
   type        = string
+  default = ""
 }
 
 variable "server_image" {
   type        = string
   description = "The Docker image for the Express server."
+  default = ""
 }
 
 variable "socket_server_image" {
   type        = string
   description = "The Docker image for the Socket.io server."
+  default = ""
 }
 
 variable "file_gateway_image" {
   type        = string
   description = "The Docker image for the file-gateway server."
+  default = ""
 }
 
 variable "client_image" {
   type        = string
   description = "The Docker image for the client."
+  default = ""
 }
 
 variable "mongo_connection_string" {
   type = string
   description = "The mongo connection string"  
+  default = ""
 }
